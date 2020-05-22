7bf53b10f3b5d9002972bc18148d4092b2e049ce
Oskari Järvelin <contact@oskarijarvelin.fi>
2020-05-22 21:45:18 +0300

    Init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+class Index extends React.Component {
+    render() {
+        return <div>
+            <Head>
+                <title>Oskari Järvelin</title>
+                <meta name="description" content="This is an example of a meta description. This will often show up in search results." />
+            </Head>
+            
+            <h1>Title</h1>
+        </div>
+    }
+}
+export default Index;
