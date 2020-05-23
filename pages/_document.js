918b380da8e4199bed946c03feeeae52900a8541
Oskari Järvelin <contact@oskarijarvelin.fi>
2020-05-23 16:52:38 +0300

    Started index

--- pages/_document.js
+++ pages/_document.js
@@ -5,40 +5,61 @@ export default class MyDocument extends Document {
         return (
             <html lang="fi">
                 <Head>
-                <link rel="manifest" href="manifest.json" />
+                    <link rel="manifest" href="manifest.json" />
 
-<meta name="mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="application-name" content="oskarijarvelin" />
-<meta name="apple-mobile-web-app-title" content="oskarijarvelin" />
-<meta name="theme-color" content="#223388" />
-<meta name="msapplication-navbutton-color" content="#223388" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-<meta name="msapplication-starturl" content="/" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                    <meta name="mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="application-name" content="oskarijarvelin" />
+                    <meta name="apple-mobile-web-app-title" content="oskarijarvelin" />
+                    <meta name="theme-color" content="#223388" />
+                    <meta name="msapplication-navbutton-color" content="#223388" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                    <meta name="msapplication-starturl" content="/" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-<link rel="icon" type="image/png" sizes="72x72" href="images/icons/icon-72x72.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="72x72" href="images/icons/icon-72x72.png" />
-<link rel="icon" type="image/png" sizes="96x96" href="images/icons/icon-96x96.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="96x96" href="images/icons/icon-96x96.png" />
-<link rel="icon" type="image/png" sizes="128x128" href="images/icons/icon-128x128.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="128x128" href="images/icons/icon-128x128.png" />
-<link rel="icon" type="image/png" sizes="144x144" href="images/icons/icon-144x144.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="144x144" href="images/icons/icon-144x144.png" />
-<link rel="icon" type="image/png" sizes="152x152" href="images/icons/icon-152x152.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="152x152" href="images/icons/icon-152x152.png" />
-<link rel="icon" type="image/png" sizes="192x192" href="images/icons/icon-192x192.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="192x192" href="images/icons/icon-192x192.png" />
-<link rel="icon" type="image/png" sizes="384x384" href="images/icons/icon-384x384.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="384x384" href="images/icons/icon-384x384.png" />
-<link rel="icon" type="image/png" sizes="512x512" href="images/icons/icon-512x512.png" />
-<link rel="apple-touch-icon" type="image/png" sizes="512x512" href="images/icons/icon-512x512.png" />
+                    <link rel="icon" type="image/png" sizes="72x72" href="images/icons/icon-72x72.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="72x72" href="images/icons/icon-72x72.png" />
+                    <link rel="icon" type="image/png" sizes="96x96" href="images/icons/icon-96x96.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="96x96" href="images/icons/icon-96x96.png" />
+                    <link rel="icon" type="image/png" sizes="128x128" href="images/icons/icon-128x128.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="128x128" href="images/icons/icon-128x128.png" />
+                    <link rel="icon" type="image/png" sizes="144x144" href="images/icons/icon-144x144.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="images/icons/icon-144x144.png" />
+                    <link rel="icon" type="image/png" sizes="152x152" href="images/icons/icon-152x152.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="152x152" href="images/icons/icon-152x152.png" />
+                    <link rel="icon" type="image/png" sizes="192x192" href="images/icons/icon-192x192.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="images/icons/icon-192x192.png" />
+                    <link rel="icon" type="image/png" sizes="384x384" href="images/icons/icon-384x384.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="384x384" href="images/icons/icon-384x384.png" />
+                    <link rel="icon" type="image/png" sizes="512x512" href="images/icons/icon-512x512.png" />
+                    <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="images/icons/icon-512x512.png" />
                 </Head>
 
                 <body>
                     <Main />
                     <NextScript />
                 </body>
+
+                <style global jsx>{`
+                    @font-face {
+                        font-family: 'Roboto Condensed';
+                        font-style: normal;
+                        font-weight: 700;
+                        src: local('Roboto Condensed Bold'), local('RobotoCondensed-Bold'),
+                            url('/fonts/roboto-condensed-v18-latin-700.woff2') format('woff2'),
+                            url('/fonts/roboto-condensed-v18-latin-700.woff') format('woff');
+                    }
+
+                    html, body {
+                        margin: 0;
+                        padding: 0;
+                        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+                    }
+
+                    h1, h2, h3, h4, h5, h6 {
+                        font-family: 'Roboto Condensed', sans-serif;
+                    }    
+                `}</style>
             </html>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,72 @@ import Head from 'next/head';
 
 class Index extends React.Component {
     render() {
-        return <div>
+        return <main>
             <Head>
                 <title>Oskari Järvelin</title>
                 <meta name="description" content="This is an example of a meta description. This will often show up in search results." />
             </Head>
             
-            <h1>Title</h1>
-        </div>
+            <div className="container">
+                <h1>Oskari Järvelin</h1>
+            </div>
+
+            <style jsx>{`
+                main {
+                    background-attachment: fixed;
+                    background-image: url(/images/oskari.jpg);
+                    background-position: 75% 10%;
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    height: 100vh;
+                    position: relative;
+                    width: 100vw;
+                    z-index: 1;
+                }
+
+                main::before {
+                    background-attachment: fixed;
+                    background-image: linear-gradient(0deg,rgba(255,255,255,1) 0,rgba(255,255,255,.5) 10%,rgba(255,255,255,0) 40%);
+                    content: '';
+                    height: 100%;
+                    left: 0;
+                    position: absolute;
+                    top: 0;
+                    width: 100%;
+                }
+
+                main::after {
+                    background-attachment: fixed;
+                    background-image: linear-gradient(18deg,rgba(255,255,255,.95) 0,rgba(255,255,255,.6) 30%,rgba(255,255,255,0) 55%,rgba(255,255,255,0) 100%);
+                    content: '';
+                    height: 100%;
+                    left: 0;
+                    position: absolute;
+                    top: 0;
+                    width: 100%;
+                }
+
+                @media (min-width: 992px) {
+                    main::before {
+                        background-image: linear-gradient(90deg,#fff 0,rgba(255,255,255,.5) 4%,rgba(255,255,255,0) 6%);
+                    }
+
+                    main::after {
+                        background-image: linear-gradient(105deg,rgba(248,248,248,.95) 0,rgba(248,248,248,.8) 32%,rgba(248,248,248,.5) 42%,rgba(248,248,248,0) 55%,rgba(248,248,248,0) 100%);
+                    }
+                }   
+
+                h1 {
+                    margin: 0;
+                }
+
+                .container {
+                    padding: 2rem;
+                    position: relative;
+                    z-index: 2;
+                }
+            `}</style>
+        </main>
     }
 }
 export default Index;
