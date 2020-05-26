61309e4faa7da62a538f5cc451bedf5fb58f9b85
Oskari Järvelin <contact@oskarijarvelin.fi>
2020-05-26 13:16:24 +0300

    Fixed application name

--- pages/_document.js
+++ pages/_document.js
@@ -20,8 +20,8 @@ export default class MyDocument extends Document {
 
                     <meta name="mobile-web-app-capable" content="yes" />
                     <meta name="apple-mobile-web-app-capable" content="yes" />
-                    <meta name="application-name" content="oskarijarvelin" />
-                    <meta name="apple-mobile-web-app-title" content="oskarijarvelin" />
+                    <meta name="application-name" content="Oskari Järvelin" />
+                    <meta name="apple-mobile-web-app-title" content="Oskari Järvelin" />
                     <meta name="theme-color" content="#ffffff" />
                     <meta name="msapplication-navbutton-color" content="#ffffff" />
                     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
